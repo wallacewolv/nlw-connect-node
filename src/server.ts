808ff174d395bf9d0074055a0fe19309f762d0a7
wallace <wallace_wolv@hotmail.com>
2025-02-22 14:56:53 -0300

    feat: Configurações iniciais

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import {
+    validatorCompiler,
+    serializerCompiler,
+    ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+
+app.post('/subscriptions', {
+    schema: {
+        body: z.object({
+            name: z.string(),
+            email: z.string().email(),
+        }),
+    },
+}, (request, reply) => {
+    const { name, email } = request.body;
+
+    // criação na inscrição no banco de dados
+
+    return reply.status(201).send({
+        name,
+        email,
+    })
+})
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('HTTP server running!')
+})
