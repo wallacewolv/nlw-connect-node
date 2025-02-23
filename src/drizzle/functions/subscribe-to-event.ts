5c2031b19b8eeaa00a2c38d3ddaccfe03da3dfe2
wallace <wallace_wolv@hotmail.com>
2025-02-23 00:53:37 -0300

    feat: Criando funcionalidade de salvar o usuario no banco

---
+++ src/drizzle/functions/subscribe-to-event.ts
@@ -0,0 +1,26 @@
+import { db } from '../client'
+import { subscriptions } from '../schema/subscriptions'
+
+interface SubscribeToEventParams {
+  name: string
+  email: string
+}
+
+export async function subscribeToEvent({
+  name,
+  email,
+}: SubscribeToEventParams) {
+  const result = await db
+    .insert(subscriptions)
+    .values({
+      name,
+      email,
+    })
+    .returning()
+
+  const subscriber = result[0]
+
+  return {
+    subscriberId: subscriber.id,
+  }
+}
